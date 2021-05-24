f3fa06ea76bb50d0ba87af2139300924138830af
KeylorCalderon <43432543+KeylorCalderon@users.noreply.github.com>
2021-05-24 01:40:56 -0600

    Ajustes en listar empleados

--- TareaProgramada1BD/TareaProgramada1BD/SP_Pagina.sql
+++ TareaProgramada1BD/TareaProgramada1BD/SP_Pagina.sql
@@ -27,6 +27,73 @@ CREATE PROCEDURE ListarUsuarios
 --EXECUTE ListarDepartamento
 GO
 
+/*
+CREATE PROCEDURE ListarSemana
+	@InIdEmpleado INT,
+	@OutResultCode INT OUTPUT
+
+	AS
+	BEGIN
+		SET NOCOUNT ON;
+		BEGIN TRY
+			DECLARE @TotalDeducciones INT, @TotalHorasNormales INT,
+			@TotalHorasExtraNormales INT, @TotalHorasExtraDobles INT;
+			SELECT
+				@TotalDeducciones=SUM(MP.Monto)
+			FROM
+				MovimientoPlanilla MP, PlanillaSemanalXEmpleado PSXE
+			WHERE
+				MP.IdSemana=PSXE.IdSemana AND PSXE.IdEmpleado=@InIdEmpleado AND
+				(MP.TipoMovimiento=4 OR MP.TipoMovimiento=5);
+
+			SELECT
+				@TotalHorasNormales=SUM(MP.Monto)
+			FROM
+				MovimientoPlanilla MP, PlanillaSemanalXEmpleado PSXE
+			WHERE
+				MP.IdSemana=PSXE.IdSemana AND PSXE.IdEmpleado=@InIdEmpleado AND
+				MP.TipoMovimiento=1;
+
+			SELECT
+				@TotalHorasExtraNormales=SUM(MP.Monto)
+			FROM
+				MovimientoPlanilla MP, PlanillaSemanalXEmpleado PSXE
+			WHERE
+				MP.IdSemana=PSXE.IdSemana AND PSXE.IdEmpleado=@InIdEmpleado AND
+				MP.TipoMovimiento=2;
+
+			SELECT
+				@TotalHorasExtraDobles=SUM(MP.Monto)
+			FROM
+				MovimientoPlanilla MP, PlanillaSemanalXEmpleado PSXE
+			WHERE
+				MP.IdSemana=PSXE.IdSemana AND PSXE.IdEmpleado=@InIdEmpleado AND
+				MP.TipoMovimiento=3;
+
+			SELECT TOP 15 PSM.Id, PSM.IdSemana, PSM.SalarioNeto, @TotalDeducciones, @TotalHorasNormales,
+			@TotalHorasExtraNormales, @TotalHorasExtraDobles
+			FROM
+				PlanillaSemanalXEmpleado PSM
+			WHERE
+				PSM.IdEmpleado=@InIdEmpleado
+			
+		END TRY
+		BEGIN CATCH
+			INSERT INTO DBErrores VALUES (
+			SUSER_SNAME(),
+			ERROR_NUMBER(),
+			ERROR_STATE(),
+			ERROR_SEVERITY(),
+			ERROR_LINE(),
+			ERROR_PROCEDURE(),
+			ERROR_MESSAGE(),
+			GETDATE()
+			)
+		END CATCH
+		SET NOCOUNT OFF;
+	END
+GO*/
+
 ---Recibe el Id del empleado, y devuelve los campos solicitados además del ID de
 --PlanillaMensualXEmpleado, este no se muestra, solo se necesita para consultar en ListarDeduccionesMes
 CREATE PROCEDURE ListarMes
@@ -46,7 +113,7 @@ CREATE PROCEDURE ListarMes
 			
 
 			SELECT TOP 12
-				PMXE.Id, PMXE.IdMes, PMXE.SalarioTotal, PMXE.SalarioNeto
+				PMXE.Id, PMXE.IdMes, PMXE.SalarioTotal, PMXE.SalarioNeto, @TotalDeducciones
 			FROM
 				PlanillaMensualXEmpleado PMXE, Empleado E
 			WHERE
